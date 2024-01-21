63c93a33b09bad2adf58fef27118f5a3bbbf0f5a
lovelyang <116940809+lovelyang@users.noreply.github.com>
2024-01-21 16:04:01 +0300

    Delete psx_d.lua

--- psx_d.lua
+++
@@ -1,6 +0,0 @@
-task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/lovelyang/roblox/main/func.lua"))()
-        end)
-task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/lovelyang/roblox/main/load.lua"))()
-        end)
